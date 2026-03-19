e271bb7f674c6ab49b1030dfa2e4be27d3a5c8c1
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-03-19 17:27:01 +0000

    Shop System
    I have created a simple shop system so I can create shops, add stock and staff.

---
+++ shop-system/index.js
@@ -0,0 +1,42 @@
+
+//Ability to create stock
+//ability to purchase items into stock
+//ability to sell stock
+
+//Products list, which has a stock quantity
+
+//Shop
+//Products / Stock - Price - Quantity
+
+class Shop{
+    constructor (shopName,products,staff,tills){
+        this.shopName = shopName;
+        this.products = [];
+        this.staff = [];
+        this.tills = tills;
+    }
+    addStaff(staff){
+        this.staff.push(staff);
+    }
+    addProducts(product){
+        this.products.push(product);
+    }
+}
+
+class Product{
+    constructor(name,price,quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+class Staff{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+let MyShop = new Shop ("MyShop",[],[],2);
+
+let kettle = new Product ("kettle",14.99,5);
+let cup = new Product ("cup",3.99,15);
