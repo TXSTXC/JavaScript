da65fdbec0b7faafd59c22daf3a7a8a9e39df662
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-04-20 15:35:32 +0100

    Amendment
    Updated folder names and added some base code to a weapon configurator.
    Set up so I am going to be including some info and an image upon selection.

---
+++ army-configurator/script.js
@@ -0,0 +1,4 @@
+
+document.querySelectorAll("p").setAttribute("class","bg-yellow"); //This would update the Class to 'bg-yellow' for all the paragraphs in the page.
+
+document.querySelectorAll("p").classList.add("bg-yellow"); //This would add the 'bg-yellow' class to all the paragraphs in the page, without removing any existing classes.
