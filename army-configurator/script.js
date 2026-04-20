44dbafe0c7953c761f255ca66cdd7c295da33d35
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-04-20 16:58:27 +0100

    Amends
    Added some more JS, not finished.

--- army-configurator/script.js
+++ army-configurator/script.js
@@ -1,4 +1,37 @@
 
-document.querySelectorAll("p").setAttribute("class","bg-yellow"); //This would update the Class to 'bg-yellow' for all the paragraphs in the page.
 
-document.querySelectorAll("p").classList.add("bg-yellow"); //This would add the 'bg-yellow' class to all the paragraphs in the page, without removing any existing classes.
+
+
+const weapon = {
+    l85A3: {
+        image: "./media/l85a3.jpg",
+        description: "The L85A3 is the latest iteration of the L85 series, featuring improved reliability and performance. It is equipped with a new barrel, enhanced gas system, and updated ergonomics, making it a formidable weapon for modern combat scenarios."
+    },
+    l403A1: {
+        image: "./media/l403a1.jpg",
+        description: "The L403A1 is a lightweight, compact weapon designed for close-quarters combat. It features a folding stock and a quick-detach sight, making it highly maneuverable in tight spaces."
+    },
+    l7A2: {
+        image: "./media/l7a2.jpg",
+        description: "The L7A2 is a versatile machine gun that provides sustained firepower for infantry units. It is equipped with a quick-change barrel and a bipod for stability during prolonged engagements."
+    }
+}
+
+
+
+
+
+
+const weaponSelection = document.getElementById('weapon-selection');
+
+weaponSelection.addEventListener('change', function(
+    
+){
+    const selectedWeapon = weaponSelection.value;
+
+    //selected weapon value would then update the img src to the correct imaage.
+
+
+
+
+})
