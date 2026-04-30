8821a7ff3e49750d98c86d9331bcf554fac9b0fb
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-04-30 14:18:05 +0100

    Updated Project
    Added new project to work on.

---
+++ esports-website/scripts.js
@@ -0,0 +1,7 @@
+
+
+
+//IDEA
+//Replicate an APP, but built within a browser.
+//Esports APP to help players matchmake and find teams to play against.
+//Categories for each GAME, with sub-pages and ways to matchmake within each.
