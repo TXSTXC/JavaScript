1bcb9fb4556502392c4964102974282cbc532b04
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-05-06 11:28:00 +0100

    Hidden UI
    Updated UI to now allow for opening & closing this.
    Updated styling to uniform the look also.

--- esports-website/scripts.js
+++ esports-website/scripts.js
@@ -1,6 +1,21 @@
 
 
 
+const findMoreButton = document.getElementById('find-more');
+
+findMoreButton.addEventListener('click', () => {
+    const gameFinderContainer = document.querySelector('.game-finder-container');
+    gameFinderContainer.classList.toggle('hidden');
+});
+
+const findGameCancelButton = document.getElementById('find-game-cancel-btn');
+findGameCancelButton.addEventListener('click', (e) => {
+    const gameFinderContainer = document.querySelector('.game-finder-container');
+    gameFinderContainer.classList.toggle('hidden');
+    e.preventDefault();
+});
+
+
 //IDEA
 //Replicate an APP, but built within a browser.
 //Esports APP to help players matchmake and find teams to play against.
