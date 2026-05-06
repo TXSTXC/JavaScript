0bdb05ced7c3fcfde67fc82ff03c8617f78a9230
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-05-06 17:27:33 +0100

    Profile - EDIT
    I can now cancel the edit screen.
    Also added ability to upload profile image and update USERNAME

--- esports-website/profile-scripts.js
+++ esports-website/profile-scripts.js
@@ -4,3 +4,8 @@ editProfileIcon.addEventListener('click', () => {
     const editProfileContainer = document.querySelector('.edit-profile-container');
         editProfileContainer.classList.toggle('hidden');
 });
+const editProfileCancelButton = document.querySelector('.edit-profile-btn-container .edit-profile-form-btn:nth-child(2)');
+editProfileCancelButton.addEventListener('click', () => {
+    const editProfileContainer = document.querySelector('.edit-profile-container');
+        editProfileContainer.classList.toggle('hidden');
+});
