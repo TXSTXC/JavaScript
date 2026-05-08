63afecd17c8c1825723d1078c730588e068ec47b
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-05-08 14:33:29 +0100

    SEARCH
    Added the UI for the Search section
    Headers
    Search Buttons
    Recent Players
    
    Need to add more content as it is not yet quite right.

--- esports-website/profile-scripts.js
+++ esports-website/profile-scripts.js
@@ -29,51 +29,61 @@ const TXSTXC = {
 };
 */
 
-const usernameInput = document.getElementById('username');
-const profilePictureInput = document.getElementById('profile-picture');
-const nameInput = document.getElementById('name');
-const ageInput = document.getElementById('age');
-const locationInput = document.getElementById('location');
+//EDIT PROFILE UI VARIABLES
 
 //UPDATE PROFILE INFO FUNCTION
+const usernameInput = document.getElementById('edit-username');
+const profilePictureInput = document.getElementById('edit-profile-picture');
+const nameInput = document.getElementById('edit-name');
+const ageInput = document.getElementById('edit-age');
+const locationInput = document.getElementById('edit-location');
+const bioInput = document.getElementById('edit-bio');
+
 updateProfileInfo = function() {
     const usernameInputValue = usernameInput.value;
     const profilePictureFile = profilePictureInput.files[0];
     const nameInputValue = nameInput.value;
     const ageInputValue = ageInput.value;
     const locationInputValue = locationInput.value;
+    const bioInputValue = bioInput.value;
 
     // Update the profile display on the page
     document.getElementById('profile-username').innerHTML = usernameInputValue;
     document.getElementById('profile-name').innerHTML = nameInputValue;
     document.getElementById('profile-age').innerHTML = `${ageInputValue}`;
     document.getElementById('profile-location').innerHTML = `${locationInputValue}`;
+    document.getElementById('profile-bio').innerHTML = bioInputValue;
     //document.getElementById('profile-member-since').innerHTML = `${TXSTXC.memberSince}`;
 };
+
+//Reset EDIT PROFILE data fields
 resetEditProfileInfo = function (){
     usernameInput.value = '';
     profilePictureInput.value = '';
     nameInput.value = '';
     ageInput.value = '';
     locationInput.value = '';
+    bioInput.value = '';
+
 }
+//POPULATE EDIT PROFILE data fields from PRFOILE DISPLAY
 populateEditProfileInfo = function() {
     usernameInput.value = document.getElementById('profile-username').innerHTML;
     nameInput.value = document.getElementById('profile-name').innerHTML;
     ageInput.value = document.getElementById('profile-age').innerHTML;
     locationInput.value = document.getElementById('profile-location').innerHTML;
+    bioInput.value = document.getElementById('profile-bio').innerHTML;
 }
 
-//GRabs the Container for the EDIT PROFILE button
+//Grabs the Container for the EDIT PROFILE button
 const editProfileContainer = document.querySelector('.edit-profile-container');
 
 //Event Listener for the Edit Profile Button and the Cancel Button
 const editProfileIcon = document.getElementById('edit-profile-icon')
 editProfileIcon.addEventListener('click', () => {
-        editProfileContainer.classList.toggle('hidden');
-        populateEditProfileInfo();
+    editProfileContainer.classList.toggle('hidden');
+    populateEditProfileInfo();
 });
-
 //Event Listener for the Save Changes Button
 const saveChangesButton = document.querySelector('.edit-profile-btn-container .edit-profile-form-btn:nth-child(1)');
 saveChangesButton.addEventListener('click', () => {
@@ -82,12 +92,19 @@ saveChangesButton.addEventListener('click', () => {
     editProfileContainer.classList.toggle('hidden');
     resetEditProfileInfo();
 });
-
 //Event Listener for the Cancel Button
 const editProfileCancelButton = document.querySelector('.edit-profile-btn-container .edit-profile-form-btn:nth-child(2)');
 editProfileCancelButton.addEventListener('click', () => {
     editProfileContainer.classList.toggle('hidden');
     resetEditProfileInfo();
 });
+
+
+
+
+
+
+
+
 
 
