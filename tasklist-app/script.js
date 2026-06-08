7c4cdb8adc4a6c5622a70d0c90dd2593067b7f5a
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-06-08 11:42:46 +0100

    To Do Task List
    Created the basis html and css for this.
    
    Also added the JS so I can add li based on the user input.

---
+++ tasklist-app/script.js
@@ -0,0 +1,26 @@
+
+
+//Grabs the TO DO list and assigns to todoList variable.
+const todoList = document.getElementById('ul-todo');
+const input = document.getElementById('input-todo');
+
+//function to add a new item into the .#ul-todo
+function addItem() {
+
+    //take the input from the input field and assign to a variable.
+    const newTask = input.value;
+
+    //create a new list item element
+    const listItem = document.createElement('li');
+    listItem.textContent = newTask;
+
+    //append the new list item to the TO DO list
+    todoList.appendChild(listItem);
+
+    //clear the input field
+    input.value = '';
+}
+
+document.getElementById('add-btn').addEventListener('click', addItem);{
+    addItem();
+}
