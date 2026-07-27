4e065c26ce69289a9e736c4d44cad8ee36d8c88b
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-07-27 17:00:23 +0100

    Added some styling, size queries and root css.

--- math/script.js
+++ math/script.js
@@ -1,5 +1,39 @@
 
 
+
+
+
+
+//Is the below correct for parameter and argument?
+myFunc(parameter);
+
+let myFunc = function (argument) {
+    console.log(y);
+    let y = 20;
+ 
+};
+
+myFunction(data1);
+function myFunction (data1){
+    let x = 10;
+    console.log(x);
+};
+
+
+function newFunction () {
+    let abc = 5;
+}
+
+let myLet = () => a + b; //Why no need for {}?
+// Best practive?
+
+
+
+
+
+
+
+/*
 const samples = [4.2, 5.1, 3.8, 4.7, 5.3];
 
 function calculationMean(samples) {
@@ -16,3 +50,4 @@ const meanRounded = Math.round(mean * 100) / 100;
 const meanMessage = `The mean of the samples is ${meanRounded}.`;
 
 console.log(meanMessage);
+*/
