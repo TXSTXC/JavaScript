b3ab1208307f9553f2e495221937233396c866f3
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-07-31 16:45:22 +0100

    Lots of bits...
    Logic updated - Not quite working as expected. All inputs are now taking clicks / changes. Logic needed to revert values when not required.

--- kids-chart/script.js
+++ kids-chart/script.js
@@ -1,20 +1,56 @@
 
 
+
+
+
 //Grab the theme of the CHART
 const chartTheme = document.getElementById('chart-theme-select');
-let themeValue = null; 
-chartTheme.addEventListener('click', () => {
-    if (chartTheme.value === "behaviour"){
-        themeValue = 1;
+let themeValue = null;
+chartTheme.addEventListener('change', function () {
+    if ((templateValue === 4) || 8){
+        themeValue = chartTheme.value === "behaviour" ? 1 : 2;
+        templateValue = templateValue + themeValue;
+        console.log(`"ThemeValue =" ${themeValue}`);
+        console.log(`"templateValue =" ${templateValue}`);
     }
-    if (chartTheme.value === "potty-training"){
-        themeValue = 2;
+    if ((templateValue === null) || 1 || 2 || 5 || 6 || 9 || 10){
+        return;
     }
-    console.log(themeValue);
 });
 
-function testThemeValue(){
-    console.log(themeValue);
-}
+//Grab the Colour Style for the chart
+const colourStyle = document.getElementById('chart-colour-select');
+let colourValue = null;
+colourStyle.addEventListener('change', function () {
+    if (templateValue === 4 || 8){
+        colourValue = colourStyle.value === "blue" ? 4 : 8;
+        templateValue = templateValue + colourValue;
+        console.log(colourValue);
+    }
+
+});
+
+//Grab the value from nameInput
+const childNameInput = document.getElementById('name-input');
+let childName = null;
+
+const previewButton = document.getElementById('pre-btn');
+const generateButton = document.getElementById('gen-btn');
+
+//This may need moving up if not working.
+let templateValue = null;
+
+previewButton.addEventListener('click', function (){
+    if (templateValue === null){
+        alert("Please complete all requirements to preview a template.")
+    }
+});
+
+
+
+
+
+
+
 
 
