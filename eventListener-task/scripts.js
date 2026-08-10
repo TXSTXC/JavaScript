66a5b0d13f3ee306b79f052c6a7e11039331caea
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-08-10 16:30:49 +0100

    Image Gallery Project
    Started and have built the base HTML & CSS.
    Starting to implement base JS now also.

---
+++ eventListener-task/scripts.js
@@ -0,0 +1,34 @@
+
+
+
+//Can this be optimised? I feel like it can be but I don't know how to do it.
+const button1 = document.getElementById("button1");
+button1.addEventListener("click", () => {
+    const imageInput = document.getElementById("file-input1");
+    addImage(imageInput.value);
+});
+const button2 = document.getElementById("button2");
+button2.addEventListener("click", () => {
+    const imageInput = document.getElementById("file-input2");
+    addImage(imageInput.value);
+});
+const button3 = document.getElementById("button3");
+button3.addEventListener("click", () => {
+    const imageInput = document.getElementById("file-input3");
+    addImage(imageInput.value);
+});
+const button4 = document.getElementById("button4");
+button4.addEventListener("click", () => {
+    const imageInput = document.getElementById("file-input4");
+    addImage(imageInput.value);
+});
+
+
+
+//CREATE THE IMAGE 'function' HERE
+function addImage(imageSlot) {
+    if(imageSlot === imageSlot){
+
+    }
+    
+}
