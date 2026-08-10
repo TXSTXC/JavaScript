cdeadc8bbcf173f161ce5d6c5f6044a3b3d61df0
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-08-10 17:00:01 +0100

    JavaScript Finished
    Base JS done to input images into the 4 gallery slots.
    Currently not allowing the files as it is unsafe? Will need to check and sort.

--- eventListener-task/scripts.js
+++ eventListener-task/scripts.js
@@ -2,22 +2,22 @@
 
 
 //Can this be optimised? I feel like it can be but I don't know how to do it.
-const button1 = document.getElementById("button1");
+const button1 = document.getElementById("add-image1");
 button1.addEventListener("click", () => {
     const imageInput = document.getElementById("file-input1");
     addImage(imageInput.value);
 });
-const button2 = document.getElementById("button2");
+const button2 = document.getElementById("add-image2");
 button2.addEventListener("click", () => {
     const imageInput = document.getElementById("file-input2");
     addImage(imageInput.value);
 });
-const button3 = document.getElementById("button3");
+const button3 = document.getElementById("add-image3");
 button3.addEventListener("click", () => {
     const imageInput = document.getElementById("file-input3");
     addImage(imageInput.value);
 });
-const button4 = document.getElementById("button4");
+const button4 = document.getElementById("add-image4");
 button4.addEventListener("click", () => {
     const imageInput = document.getElementById("file-input4");
     addImage(imageInput.value);
@@ -26,9 +26,23 @@ button4.addEventListener("click", () => {
 
 
 //CREATE THE IMAGE 'function' HERE
-function addImage(imageSlot) {
-    if(imageSlot === imageSlot){
-
+function addImage(attachment, imageOption) {
+    if(attachment === ""){
+        alert("Please select an image to upload.");
     }
-    
-}
+    if(imageOption === "option1"){
+        imageOption.src = attachment;
+    }
+    if(imageOption === "option2"){
+        imageOption.src = attachment;
+    }
+    if(imageOption === "option3"){
+        imageOption.src = attachment;
+    }
+    if(imageOption === "option4"){
+        imageOption.src = attachment;
+    }
+    else{
+        alert("something unexpected happened... Hmm... Please try again.");
+    }
+};
