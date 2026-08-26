a4c099ccc4ec8976eb35d80b0c006b7536640080
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-08-26 16:29:22 +0100

    Local Storage
    I now have my array storing data in local storage, and I am able to call it back and manipulate the Array.

--- portfolio-checkpoint/scripts.js
+++ portfolio-checkpoint/scripts.js
@@ -1,7 +1,8 @@
 
+//local storage MDN link - https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
 
 //FORM SUBMISSION LOGIC
-const entryList = [];
+let entryList = JSON.parse(localStorage.getItem('userEntries')) || [];
 
 const entryForm = document.querySelector('.form-container');
 entryForm.addEventListener('submit', (event) =>{
@@ -9,6 +10,7 @@ entryForm.addEventListener('submit', (event) =>{
 
     const entrySubject = document.getElementById('entrySubject').value;
     const entryDesc = document.getElementById('entryDesc').value;
+    const entryDate = ;
 
     const myEntry = {
         subject: entrySubject,
@@ -17,13 +19,25 @@ entryForm.addEventListener('submit', (event) =>{
 
     entryList.push(myEntry);
 
-     localStorage.setItem('entries', JSON.stringify(entryList))
+     localStorage.setItem('userEntries', JSON.stringify(entryList))
+     let whatIsThis = JSON.parse(localStorage.getItem("userEntries"));
+
+     console.log(whatIsThis);
+
+     const homeEntries = entryList.slice(-5).reverse(); //Had to Google this as (0,5) was always giving me the first 5, not the last 5.
 
     entryForm.reset();
 });
 
 
+
 //HOME PAGE ARRAY LOGIC
+ 
+
+homeEntries.map((x) => {
+    //Need to access each value from the object keys.
+    const entrySubject =  x.subject;
+    const entryDesc = x.description;
 
 
-
+});
